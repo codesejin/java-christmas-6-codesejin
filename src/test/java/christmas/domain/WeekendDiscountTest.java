9a3e9c3c3ff55e8aa5ecc29d9c68f9caa7eb17e8
sejin park <seijin0722@naver.com>
2023-11-13 22:12:19 +0900

    test(WeekendDiscountTest) : 주말 할인 적용 테스트

---
+++ src/test/java/christmas/domain/WeekendDiscountTest.java
@@ -0,0 +1,52 @@
+package christmas.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static christmas.utils.Constants.DEFAULT_AMOUNT;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class WeekendDiscountTest {
+    @Test
+    @DisplayName("주말 할인 - 주말에 여러 주문했을 경우 ( 메인요리 1개당 2023원할인 )")
+    void createWeekdayDiscount_severalOrder() {
+        Map<String, Integer> orderItems = new HashMap<>();
+        orderItems.put("아이스크림", 1);
+        orderItems.put("바비큐립", 1); // main
+        orderItems.put("레드와인", 1);
+        Order order = Order.create(orderItems);
+
+        VisitDay weekdayVisitDay = VisitDay.create(2);// 토요일
+        WeekendDiscount weekendDiscount = WeekendDiscount.create(order, weekdayVisitDay);
+
+        assertThat(weekendDiscount.getDiscountAmount()).isEqualTo(1 * 2023);
+    }
+    @Test
+    @DisplayName("주말 할인 - 주말에 메인요리 2개 주문했을 경우 ( 메인요리 1개당 2023원할인 )")
+    void createWeekdayDiscount_TwoDessert() {
+        Map<String, Integer> orderItems = new HashMap<>();
+        orderItems.put("해산물파스타", 2);  // main
+        Order order = Order.create(orderItems);
+
+        VisitDay weekdayVisitDay = VisitDay.create(1);// 금요일
+        WeekendDiscount weekendDiscount = WeekendDiscount.create(order, weekdayVisitDay);
+
+        assertThat(weekendDiscount.getDiscountAmount()).isEqualTo(2 * 2023);
+    }
+
+    @Test
+    @DisplayName("주말 할인 - 평일에 메인요리 2개 주문했을 경우 할인 없음")
+    void createWeekdayDiscount_NoWeekDay() {
+        Map<String, Integer> orderItems = new HashMap<>();
+        orderItems.put("해산물파스타", 2);  // main
+        Order order = Order.create(orderItems);
+
+        VisitDay weekDayVisitDay = VisitDay.create(3);// 일요일
+        WeekendDiscount weekendDiscount = WeekendDiscount.create(order, weekDayVisitDay);
+
+        assertThat(weekendDiscount.getDiscountAmount()).isEqualTo(DEFAULT_AMOUNT);
+    }
+}
