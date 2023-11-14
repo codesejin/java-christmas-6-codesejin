cd560a283fc185751b4eb25d5561552ab496c9d8
sejin park <seijin0722@naver.com>
2023-11-14 12:40:46 +0900

    chore(all) : 불필요한 내용 제거

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -5,7 +5,7 @@ import christmas.controller.Promotion;
 public class Application {
     public static void main(String[] args) {
         Promotion promotion = new Promotion();
-        promotion.start();;
-        promotion.running();;
+        promotion.start();
+        promotion.running();
     }
 }
